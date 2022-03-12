ba9834f84668f84739e48ba25f85115de7864948
shaigit-cs <100363783+shaigit-cs@users.noreply.github.com>
2022-03-12 08:50:38 +0000

    Create GoogleHomePage.java

---
+++ src/test/java/GoogleHomePage.java
@@ -0,0 +1,39 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePageTest {
+	
+	private WebDriver driver; 
+	String appURL = "http://google.com";
+	public String baseUrl = "https://www.google.com/";
+	String driverPath = "chromedriver.exe";
+	
+	@BeforeClass
+	public void testSetUp() {
+		System.out.println("launching Chrome browser");
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+	}
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		driver.navigate().to(appURL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Google");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
